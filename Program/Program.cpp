7d6903e13046931335b615c06de5699eacbed143
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-11-12 21:44:23 +0900

    (2024.11.12) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+template <typename T>
+class SingleLinkedList
+{
+private :
+	struct Node
+	{
+		T data;
+		Node * next;
+	};
+
+	int size;
+	Node * head;
+
+public:
+
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+	}
+
+	void PushFront(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node;
+
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node * newNode = new Node;
+
+			newNode->data = data;			  
+			newNode->next = head;
+
+			head = newNode;
+		}
+
+		size++;
+	}
+};
+
+int main()
+{
+	SingleLinkedList<int> singleLinkedList;
+
+	singleLinkedList.PushFront(10);
+	singleLinkedList.PushFront(20); // [20] <----- [10]
+
+
+}
