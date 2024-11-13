77ff48f1dc351b973c40b55b86530eaca5797b01
Unity2033 <kileab@naver.com>
2024-11-13 21:45:50 +0900

    (2024.11.13) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 template <typename T>
 class SingleLinkedList
 {
@@ -42,6 +44,65 @@ public:
 
 		size++;
 	}
+
+	void PopFront()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		else
+		{
+			Node * deleteNode = head;
+
+			head = deleteNode->next;
+
+			delete deleteNode;
+
+			size--;
+		}
+	}
+
+	void PushBack(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node;
+
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node* currentNode = head;
+
+			while (currentNode->next != nullptr)
+			{
+				currentNode = currentNode->next;
+			}
+
+			Node * newNode = new Node;
+
+			currentNode->next = newNode;
+
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+
+		size++;
+	}
+
+	void Show()
+	{
+		Node * currentNode = head;
+
+		while (currentNode != nullptr)
+		{
+			cout << currentNode->data << " ";
+		
+			currentNode = currentNode->next;
+		}
+	}
 };
 
 int main()
@@ -49,7 +110,16 @@ int main()
 	SingleLinkedList<int> singleLinkedList;
 
 	singleLinkedList.PushFront(10);
-	singleLinkedList.PushFront(20); // [20] <----- [10]
+	singleLinkedList.PushFront(20); 
+
+	singleLinkedList.PushBack(5);
+	singleLinkedList.PushBack(0);
+
+	singleLinkedList.Show();
+
+	singleLinkedList.PopFront();
+	singleLinkedList.PopFront();
 
 
+	return 0;
 }
