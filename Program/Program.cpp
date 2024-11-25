1eadccdad223d36c34b973fffffae73658518fa6
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-11-25 21:42:05 +0900

    (2024.11.25) 해시 테이블

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,100 +2,90 @@
 
 using namespace std;
 
-template<typename T>
-class Vector
+class String
 {
 private:
 	int size;
-	int capacity;
+	char * container;
 
-	T * container;
 public:
-	Vector()
+	String()
 	{
 		size = 0;
-		capacity = 0;
-
 		container = nullptr;
 	}
 
-	void Resize(int newSize)
+	void operator = (const char * word)
 	{
-		// 1. capacity에 새로운 size값을 저장합니다.
-		capacity = newSize;
+		int arraySize = strlen(word) + 1;
 
-		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
-		//    메모리 공간을 가리키도록 합니다.
-		T * newContainer = new T[capacity];
+		size = strlen(word);
 
-		// 3. 새로운 메모리 공간의 값을 초기화합니다.
-		for (int i = 0; i < capacity; i++)
+		if (container == nullptr)
 		{
-			newContainer[i] = NULL;
+			container = new char[arraySize];
+
+			for (int i = 0; i < arraySize; i++)
+			{
+				container[i] = word[i];
+			}
+		}
+		else
+		{
+			char * newContainer = new char[arraySize];
+
+			for (int i = 0; i < arraySize; i++)
+			{
+				newContainer[i] = word[i];
+			}
+
+			delete container;
+
+			container = newContainer;
 		}
 
-		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열
-		//    에 넣어줍니다.
-		for (int i = 0; i < size; i++)
-		{
-			newContainer[i] = container[i];
-		}
 
-		// 5. 기존 배열의 메모리를 해제합니다.
+	}
+
+	char operator [ ] (const int & index)
+	{
+		return container[index];
+	}
+
+	int & Size()
+	{
+		return size;
+	}
+
+	~String()
+	{
 		if (container != nullptr)
 		{
 			delete [ ] container;
 		}
-		
-		// 6. 기존에 배열을 가리키던 포인터 변수의 값을
-		//    새로운 배열의 시작 주소로 가리킵니다.
-		container = newContainer;
 	}
 
-	void PushBack(T data)
-	{
-		if (capacity <= 0)
-		{
-			Resize(1);
-		}
-		else if(size >= capacity)
-		{
-			Resize(capacity * 2);
-		}
-
-		container[size++] = data;
-	}
-
-	void PopBack()
-	{
-		if (size <= 0)
-		{
-			cout << "Vector is Empty" << endl;
-		}
-		else
-		{ 
-			container[--size] = NULL;
-		}
-	}
-
-	~Vector()
-	{
-		if (container != nullptr)
-		{
-			delete [] container;
-		}
-	}
 };
 
 int main()
 {
-	Vector<int> vector;
+	String string;
 
-	vector.PushBack(10);
-	vector.PushBack(20);
+	string = "Janna";
 
-	vector.PopBack();
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i];
+	}
 
+	cout << endl;
+
+	string = "Bard";
+
+	for (int i = 0; i < string.Size(); i++)
+	{
+		cout << string[i];
+	}
 
 	return 0;
 }
