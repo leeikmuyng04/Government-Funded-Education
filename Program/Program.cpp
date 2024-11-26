b65e83f94edb2c0870ce7ac52c24e0a48818cc3e
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-11-26 21:45:07 +0900

    (2024.11.26) 완전 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -47,6 +47,74 @@ public:
 
 	}
 
+	void Append(const char * word)
+	{
+		int newSize = strlen(container) + strlen(word);
+
+		char * newContainer = new char[newSize];
+
+		for (int i = 0; i < strlen(container); i++)
+		{
+			newContainer[i] = container[i];
+		}
+
+		for (int i = 0; i < strlen(word); i++)
+		{
+			newContainer[strlen(container)+ i] = word[i];
+		}
+
+		size = newSize;
+
+		delete [] container;
+
+		container = newContainer;
+	}
+
+	int Compare(const char* word)
+	{
+		int count = 0;
+
+		for (int i = 0; i < strlen(word); i++)
+		{
+			if (container[i] != word[i])
+			{
+				break;
+			}
+			else
+			{
+				count++;
+			}
+		}
+
+		if (strlen(word) == count)
+		{
+			return 0;
+		}
+
+		int classString = 0;
+		int otherString = 0;
+
+		for (int i = 0; i < strlen(container); i++)
+		{
+			classString += container[i];
+		}
+		
+		for (int i = 0; i < strlen(word); i++)
+		{
+			otherString += word[i];
+		}
+
+		if (classString > otherString)
+		{
+			return 1;
+		}
+		else
+		{
+			return -1;
+		}
+
+	}
+
 	char operator [ ] (const int & index)
 	{
 		return container[index];
@@ -81,6 +149,8 @@ int main()
 	cout << endl;
 
 	string = "Bard";
+
+	string.Append(" Character");
 
 	for (int i = 0; i < string.Size(); i++)
 	{
