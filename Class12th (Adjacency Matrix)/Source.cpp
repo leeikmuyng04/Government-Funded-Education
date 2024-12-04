dee35a1ff015704a4ecc8f7b71efb5163f76ceda
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-04 21:44:24 +0900

    (2024.12.4) 그래프 (인접 리스트)

---
+++ Class12th (Adjacency Matrix)/Source.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 10
+
+template <typename T>
+class AdjancencyMatrix
+{
+private:
+	int size;			    // 정점의 개수
+	T vertex[SIZE];			// 정점의 집합
+	int matrix[SIZE][SIZE];	// 인접 행렬
+public:
+	AdjancencyMatrix()
+	{
+		size = 0;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			vertex[i] = NULL;
+
+			for (int j = 0; j < SIZE; j++)
+			{
+				matrix[i][j] = NULL;
+			}
+		}
+	}
+
+	void Insert(T data)
+	{
+		if (size >= SIZE)
+		{
+			cout << "Adjancency Matrix Overflow" << endl;
+		}
+		else
+		{
+			vertex[size++] = data;
+		}
+	}
+
+	void Connect(int i, int j)
+	{
+		if (size <= 0)
+		{
+			cout << "Adjancency Matrix is Empty" << endl;
+		}
+		else if (i >= size || j >= size)
+		{
+			cout << "Index Out of Range" << endl;
+		}
+		else
+		{
+			matrix[i][j] = 1;
+			matrix[j][i] = 1;
+		}
+	}
+
+	void Show()
+	{
+		if (size <= 0)
+		{
+			cout << "Adjancency Matrix is Empty" << endl;
+		}
+		else
+		{
+			cout << "  ";
+
+			for (int i = 0; i < size; i++)
+			{
+				cout << vertex[i] << " ";
+			}
+
+			cout << endl;
+
+			for (int i = 0; i < size; i++)
+			{
+				cout << vertex[i] << " ";
+
+				for (int j = 0; j < size; j++)
+				{
+					cout << matrix[i][j] << " ";
+				}
+
+				cout << endl;
+			}
+		}
+	}
+};
+
+
+int main()
+{
+	AdjancencyMatrix<char> adjancencyMatrix;
+
+	adjancencyMatrix.Insert('A');
+	adjancencyMatrix.Insert('B');
+	adjancencyMatrix.Insert('C');
+	adjancencyMatrix.Insert('D');
+
+	adjancencyMatrix.Connect(0, 1);
+	adjancencyMatrix.Connect(0, 2);
+	adjancencyMatrix.Connect(1, 2);
+	adjancencyMatrix.Connect(2, 3);
+
+
+	adjancencyMatrix.Show();
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -5,34 +5,68 @@ using namespace std;
 #define SIZE 10
 
 template <typename T>
-class AdjancencyMatrix
+class AdjacencyList
 {
 private:
-	int size;			    // 정점의 개수
-	T vertex[SIZE];			// 정점의 집합
-	int matrix[SIZE][SIZE];	// 인접 행렬
+	struct Node
+	{
+		T data;
+		Node * next;
+
+		Node(T data, Node * link = nullptr)
+		{
+			this->data = data;
+			next = link;
+		}
+	};
+
+	int size; // 정점의 개수
+	T vertex[SIZE]; // 정점의 집합
+	Node* list[SIZE]; // 인접 리스트
 public:
-	AdjancencyMatrix()
+
+	AdjacencyList()
 	{
 		size = 0;
 
 		for (int i = 0; i < SIZE; i++)
 		{
+			list[i] = NULL;
 			vertex[i] = NULL;
-
-			for (int j = 0; j < SIZE; j++)
-			{
-				matrix[i][j] = NULL;
-			}
 		}
 	}
 
+	void Insert(T data)
+	{
+		if (size >= SIZE)
+		{
+			cout << "Adjacency List Overflow" << endl;
+		}
+		else
+		{
+			vertex[size++] = data;
+		}
+	}
+
+	~AdjacencyList()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			if (list[i] != nullptr)
+			{
+				delete [] list[i];
+			}
+		}
+
+	}
 
 };
 
-
 int main()
 {
+	AdjacencyList<char> adjacencyList;
+
+
 
 	return 0;
 }
