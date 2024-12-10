035a4988d4fa9331863bfb88402f7d7b7b03259d
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-12-10 21:44:42 +0900

    (2024.12.10) 트리의 순회

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -72,6 +72,110 @@ public:
 
 	}
 
+	void Inorder(Node * root)
+	{
+		if (root != nullptr)
+		{
+			Inorder(root->left);
+			cout << root->data << " ";
+			Inorder(root->right);
+		}
+	}
+
+	bool Find(T data)
+	{
+		Node * currentNode = root;
+
+		if (currentNode == nullptr)
+		{
+			return false;
+		}
+		else
+		{
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data == data)
+				{
+					return true;
+				}
+				else
+				{
+					if (currentNode->data > data)
+					{
+						currentNode = currentNode->left;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+			}
+
+			return false;
+		}
+	}
+
+	void Remove(T data)
+	{
+		if (root == nullptr)
+		{
+			cout << "Binary Search Tree is Empty" << endl;
+		}
+		else
+		{
+			Node * currentNode = root;
+			Node * parentNode = nullptr;
+
+			while (currentNode != nullptr && currentNode->data != data)
+			{
+				if (currentNode->data > data)
+				{
+					parentNode = currentNode;
+					currentNode = currentNode->left;
+				}
+				else
+				{
+					parentNode = currentNode;
+					currentNode = currentNode->right;
+				}
+			}
+
+			if (currentNode == nullptr)
+			{
+				cout << "Data Not found in the Binary Search Tree" << endl;
+			}
+			else if(currentNode->left == nullptr && currentNode->right == nullptr)
+			{
+				if (parentNode != nullptr)
+				{
+					if (parentNode->left == currentNode)
+					{
+						parentNode->left = nullptr;
+					}
+					else
+					{
+						parentNode->right = nullptr;
+					}
+				}
+				else
+				{
+					root = nullptr;
+				}
+			}
+		}
+
+	}
+
+	void Destroy(Node * root)
+	{
+		if (root != nullptr)
+		{
+			Destroy(root->left);
+			Destroy(root->right);
+			delete root;
+		}
+	}
+
 	Node * CreateNode(T data)
 	{
 		Node* newNode = new Node();
@@ -84,13 +188,25 @@ public:
 
 		return newNode;
 	}
+
+	~BinarySearchTree()
+	{
+		Destroy(root);
+	}
 };
 
 int main()
 {
 	BinarySearchTree<int> binarySearchTree;
 
+	binarySearchTree.Insert(10);
+	binarySearchTree.Insert(15);
+	binarySearchTree.Insert(7);
+	binarySearchTree.Insert(8);
 
+	cout << binarySearchTree.Find(6) << endl;
+
+	binarySearchTree.Inorder(binarySearchTree.RootNode());
 
 	return 0;
 }
