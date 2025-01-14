9a3bd708b80fd5600c460703d7c433d79f8d03e9
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-01-14 15:44:37 +0900

    (2025.1.14) 거품 정렬

--- Government-Funded-Education/Program/Program.cpp
+++ Government-Funded-Education/Program/Program.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+bool IsPrime(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
 int main()
 {
+	cout << IsPrime(5) << endl;
 
 	return 0;
 }
