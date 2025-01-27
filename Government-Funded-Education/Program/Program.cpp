930adc61c0d88e4b3ffccee8eb1402e500f458ab
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-01-27 15:43:40 +0900

    (2025.1.27) 문자열 매칭

--- Government-Funded-Education/Program/Program.cpp
+++ Government-Funded-Education/Program/Program.cpp
@@ -2,8 +2,97 @@
 
 using namespace std;
 
+void Naive(const char * key, const char * other)
+{
+	int n = strlen(key);
+	int m = strlen(other);
+
+	int j = 0;
+
+	for (int i = 0; i < n - m; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			if (key[i + j] != other[j])
+			{
+				break;
+			}
+		}
+
+		if (j == m)
+		{
+			cout << "Pattern Found at Index : " << i << endl;
+		}
+	}
+
+}
+
+void RabinKarp(const char * pattern, const char * text)
+{
+	int patternSize = strlen(pattern);
+	int textSize = strlen(text);
+
+	int patternHash = 0;
+	int hash = 0;
+	int power = 1;
+
+	for (int i = 0; i <= patternSize - textSize; i++)
+	{
+		if (i == 0)
+		{
+			for (int j = 0; j < textSize; j++)
+			{
+				patternHash = patternHash + pattern[textSize - 1 - j] * power;
+
+				hash = hash + text[textSize - 1 - j] * power;
+
+				if (j < textSize - 1)
+				{
+					power = power * 2;
+				}
+			}
+		}
+		else
+		{
+			patternHash = 2 * (patternHash - pattern[i - 1] * power) + pattern[textSize - 1 + i];
+		}
+
+		if (patternHash == hash)
+		{
+			bool flag = true;
+
+			for (int j = 0; j < textSize; j++)
+			{
+				if (pattern[i + j] != text[j])
+				{
+					flag = false;
+
+					break;
+				}
+			}
+
+			if (flag == true)
+			{
+				cout << i << "번째에서 발견하였습니다." << endl;
+			}
+		}
+	}
+
+
+}
+
 int main()
 {
+#pragma region 문자열 매칭
+	// 문자열에 해싱 기법을 사용하여 해시 값으로
+	// 비교하는 문자열 알고리즘입니다.
+
+	// Naive("AABAAAB", "AA");
+	// "ABC"
+	RabinKarp("BCABCD", "ABC");
+
+#pragma endregion
+
 
 	return 0;
 }
